b5f01fd14fd0b0bff62444932dbcb971c703fad7
mikolajjkrol <mikolajkock@gmail.com>
2025-03-23 11:24:02 +0100

    deploy now works for me

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ echo 'Building app'
 npm run build
 
 echo 'Deploying files to server'
-scp -r dist/* m@192.168.33.11:/var/www/react/
+scp -r dist/* m@192.168.33.11:/var/www/react/task-manager/
 echo 'Done!'
