e9277e8eefdfc0de219577279eda4ed5e867213c
mikolajjkrol <mikolajkock@gmail.com>
2025-03-23 10:43:36 +0100

    deploy (ubuntu server)

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+echo 'Building app'
+npm run build
+
+echo 'Deploying files to server'
+scp -r dist/* m@192.168.33.11:/var/www/react/
+echo 'Done!'
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+const base = isGitHubPages ? '/task-manager/' : '/';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/task-manager",
+  base: base,
 })
