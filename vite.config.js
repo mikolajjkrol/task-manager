31de9d9678e2e2478ec1ff7ac702d68fa162202e
mikolajjkrol <mikolajkock@gmail.com>
2025-03-23 10:53:31 +0100

    changes in viteconfigjs

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isGitHubPages = process.env.GITHUB_PAGES === 'true';
-const base = isGitHubPages ? '/task-manager/' : '/';
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: base,
+  base: process.env.NODE_ENV === 'production' ? '/task-manager/' : '/',
 })
