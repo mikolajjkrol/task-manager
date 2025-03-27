6eda67b84264e8aff6d88bedf6bd935c38b99f80
mikolajjkrol <mikolajkock@gmail.com>
2025-03-27 20:20:00 +0100

    my ubuntu address

--- src/scripts/http.js
+++ src/scripts/http.js
@@ -1,5 +1,5 @@
 export async function fetchData(){
-    const response = await fetch('http://localhost:3000/tasks');
+    const response = await fetch('http://192.168.33.11:3000/tasks');
     const resData = await response.json();
     
     if(!response.ok){
@@ -10,7 +10,7 @@ export async function fetchData(){
 }
 
 export async function updateData(data) {
-    const response = await fetch('http://localhost:3000/tasks',{
+    const response = await fetch('http://192.168.33.11:3000/tasks',{
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data),
